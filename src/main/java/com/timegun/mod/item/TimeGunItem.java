f1e2114f4a0ca63b77576588868eeceb3a988ac7
Sheikh <m.s.thephysicsboy@gmail.com>
2026-08-19 21:28:53 +0530

    Added Time Gun Item

---
+++ src/main/java/com/timegun/mod/item/TimeGunItem.java
@@ -0,0 +1,9 @@
+package com.timegun.mod.item; // Make sure this matches your actual package
+
+import net.minecraft.world.item.Item;
+
+public class TimeGunItem extends Item {
+    public TimeGunItem(Properties properties) {
+        super(properties);
+    }
+}
